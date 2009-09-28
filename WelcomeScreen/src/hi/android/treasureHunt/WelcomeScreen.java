32e67b2c0116bdd168dc15705793ec369196a587
mannsi1983 <mannsi1983@cbe9edc2-ac26-11de-97f0-97815e7b2756>
2009-09-28 15:13:28 +0000

    This is the welcome screen. Doesn

---
+++ WelcomeScreen/src/hi/android/treasureHunt/WelcomeScreen.java
@@ -0,0 +1,28 @@
+package hi.android.treasureHunt;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class WelcomeScreen extends Activity {
+    
+	private static final int ADVANCED_ID = Menu.FIRST;
+	private static final int HELP_ID = Menu.FIRST + 1;
+	private static final int SETTINGS_ID = Menu.FIRST + 2;
+	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcomescreen);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+    	super.onCreateOptionsMenu(menu);
+    	menu.add(0,ADVANCED_ID,0,R.string.menuAdvanced);
+    	menu.add(0,HELP_ID,0,R.string.menuHelp);
+    	menu.add(0,SETTINGS_ID,0,R.string.menuSettings);
+    	return true;
+    }
+}
